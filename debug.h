09dc93d969fdfa1952cd587cbdc1b470c92fa06f
asd5fg4742-bit <asd5fg474.2@gmail.com>
2026-09-16 15:08:54 +0330

    Add files via upload

--- debug.h
+++ debug.h
@@ -1,142 +1,99 @@
-/**
- * @file debug.h
- * @author Ambroz Bizjak <ambrop7@gmail.com>
+/*
+ * Copyright (c) 2001-2004 Swedish Institute of Computer Science.
+ * All rights reserved. 
  * 
- * @section LICENSE
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the lwIP TCP/IP stack.
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the author nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * @section DESCRIPTION
- * 
- * Debugging macros.
+ * Author: Adam Dunkels <adam@sics.se>
+ *
  */
+#ifndef __LWIP_DEBUG_H__
+#define __LWIP_DEBUG_H__
 
-/**
- * @def DEBUG
- * 
- * Macro for printing debugging text. Use the same way as printf,
- * but without a newline.
- * Prepends "function_name: " and appends a newline.
+#include "lwip/arch.h"
+#include "lwip/opt.h"
+
+/** lower two bits indicate debug level
+ * - 0 all
+ * - 1 warning
+ * - 2 serious
+ * - 3 severe
  */
+#define LWIP_DBG_LEVEL_ALL     0x00
+#define LWIP_DBG_LEVEL_OFF     LWIP_DBG_LEVEL_ALL /* compatibility define only */
+#define LWIP_DBG_LEVEL_WARNING 0x01 /* bad checksums, dropped packets, ... */
+#define LWIP_DBG_LEVEL_SERIOUS 0x02 /* memory allocation failures, ... */
+#define LWIP_DBG_LEVEL_SEVERE  0x03
+#define LWIP_DBG_MASK_LEVEL    0x03
 
-/**
- * @def ASSERT_FORCE
- * 
- * Macro for forced assertions.
- * Evaluates the argument and terminates the program abnormally
- * if the result is false.
+/** flag for LWIP_DEBUGF to enable that debug message */
+#define LWIP_DBG_ON            0x80U
+/** flag for LWIP_DEBUGF to disable that debug message */
+#define LWIP_DBG_OFF           0x00U
+
+/** flag for LWIP_DEBUGF indicating a tracing message (to follow program flow) */
+#define LWIP_DBG_TRACE         0x40U
+/** flag for LWIP_DEBUGF indicating a state debug message (to follow module states) */
+#define LWIP_DBG_STATE         0x20U
+/** flag for LWIP_DEBUGF indicating newly added code, not thoroughly tested yet */
+#define LWIP_DBG_FRESH         0x10U
+/** flag for LWIP_DEBUGF to halt after printing this debug message */
+#define LWIP_DBG_HALT          0x08U
+
+#ifndef LWIP_NOASSERT
+#define LWIP_ASSERT(message, assertion) do { if(!(assertion)) \
+  LWIP_PLATFORM_ASSERT(message); } while(0)
+#else  /* LWIP_NOASSERT */
+#define LWIP_ASSERT(message, assertion) 
+#endif /* LWIP_NOASSERT */
+
+/** if "expression" isn't true, then print "message" and execute "handler" expression */
+#ifndef LWIP_ERROR
+#define LWIP_ERROR(message, expression, handler) do { if (!(expression)) { \
+  LWIP_PLATFORM_ASSERT(message); handler;}} while(0)
+#endif /* LWIP_ERROR */
+
+#ifdef LWIP_DEBUG
+/** print debug message only if debug message type is enabled...
+ *  AND is of correct type AND is at least LWIP_DBG_LEVEL
  */
+#define LWIP_DEBUGF(debug, message) do { \
+                               if ( \
+                                   ((debug) & LWIP_DBG_ON) && \
+                                   ((debug) & LWIP_DBG_TYPES_ON) && \
+                                   ((s16_t)((debug) & LWIP_DBG_MASK_LEVEL) >= LWIP_DBG_MIN_LEVEL)) { \
+                                 LWIP_PLATFORM_DIAG(message); \
+                                 if ((debug) & LWIP_DBG_HALT) { \
+                                   while(1); \
+                                 } \
+                               } \
+                             } while(0)
 
-/**
- * @def ASSERT
- * 
- * Macro for assertions.
- * The argument may or may not be evaluated.
- * If the argument is evaluated, it must not evaluate to false.
- */
+#else  /* LWIP_DEBUG */
+#define LWIP_DEBUGF(debug, message) 
+#endif /* LWIP_DEBUG */
 
-/**
- * @def ASSERT_EXECUTE
- * 
- * Macro for always-evaluated assertions.
- * The argument is evaluated.
- * The argument must not evaluate to false.
- */
+#endif /* __LWIP_DEBUG_H__ */
 
-/**
- * @def DEBUG_ZERO_MEMORY
- * 
- * If debugging is enabled, zeroes the given memory region.
- * First argument is pointer to the memory region, second is
- * number of bytes.
- */
-
-/**
- * @def WARN_UNUSED
- * 
- * Tells the compiler that the result of a function should not be unused.
- * Insert at the end of the declaration of a function before the semicolon.
- */
-
-/**
- * @def B_USE
- * 
- * This can be used to suppress warnings about unused variables. It can
- * be applied to a variable or any expression. It does not evaluate the
- * expression.
- */
-
-#ifndef BADVPN_MISC_DEBUG_H
-#define BADVPN_MISC_DEBUG_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <assert.h>
-
-#define DEBUG(...) \
-    { \
-        fprintf(stderr, "%s: ", __FUNCTION__); \
-        fprintf(stderr, __VA_ARGS__); \
-        fprintf(stderr, "\n"); \
-    }
-
-#define ASSERT_FORCE(e) \
-    { \
-        if (!(e)) { \
-            fprintf(stderr, "%s:%d Assertion failed\n", __FILE__, __LINE__); \
-            abort(); \
-        } \
-    }
-
-#ifdef NDEBUG
-    #define DEBUG_ZERO_MEMORY(buf, len) {}
-    #define ASSERT(e) {}
-    #define ASSERT_EXECUTE(e) { (e); }
-#else
-    #define DEBUG_ZERO_MEMORY(buf, len) { memset((buf), 0, (len)); }
-    #ifdef BADVPN_USE_C_ASSERT
-        #define ASSERT(e) { assert(e); }
-        #define ASSERT_EXECUTE(e) \
-            { \
-                int _assert_res = !!(e); \
-                assert(_assert_res); \
-            }
-    #else
-        #define ASSERT(e) ASSERT_FORCE(e)
-        #define ASSERT_EXECUTE(e) ASSERT_FORCE(e)
-    #endif
-#endif
-
-#ifdef __GNUC__
-    #define WARN_UNUSED __attribute__((warn_unused_result))
-#else
-    #define WARN_UNUSED
-#endif
-
-#define B_USE(expr) (void)(sizeof((expr)));
-
-#define B_ASSERT_USE(expr) { ASSERT(expr) B_USE(expr) }
-
-#endif
